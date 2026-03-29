b8e83848871c4a48060983781c38724da835ecad
ariztaufani <ariztaufanii@gmail.com>
2026-03-30 01:18:52 +0700

    commit message

--- TP_MODUL5_103022400043/Program.cs
+++ TP_MODUL5_103022400043/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TP_MODUL5_103022400043
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HaloGeneric halo = new HaloGeneric();
+            halo.SapaUser<string>("Ariz");
+        }
+    }
+}
--- TP_MODUL5_103022400043/obj/Debug/net10.0/TP_MODUL5_103022400043.AssemblyInfo.cs
+++ TP_MODUL5_103022400043/obj/Debug/net10.0/TP_MODUL5_103022400043.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL5_103022400043")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+230418efd5193ee08e5d607c417e15af0dec3586")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL5_103022400043")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL5_103022400043")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

eecd76fde1944169ede32c5353038c9668ee71a5
ariztaufani <ariztaufanii@gmail.com>
2026-03-30 01:38:34 +0700

    commit message

---
+++ TP_MODUL5_103022400043/DataGeneric.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_MODUL5_103022400043
+{
+    internal class DataGeneric<T>
+    {
+        public T data;
+
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + data);
+        }
+    }
+}
--- TP_MODUL5_103022400043/Program.cs
+++ TP_MODUL5_103022400043/Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using TP_MODUL5_103022400043;
+
+DataGeneric<string> data = new DataGeneric<string>("103022400043");
+data.PrintData();
